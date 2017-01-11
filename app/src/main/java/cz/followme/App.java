babcd8606f6fa9aa5bb64fcb92e9c5f532acda50
Jakub Begera <jakub@begera.cz>
2017-01-11 20:10:05 +0100

    changed server url

--- app/src/main/java/cz/followme/App.java
+++ app/src/main/java/cz/followme/App.java
@@ -3,6 +3,8 @@ package cz.followme;
 import android.support.multidex.MultiDexApplication;
 
 import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.answers.Answers;
+import com.crashlytics.android.core.CrashlyticsCore;
 
 import cz.followme.injection.AppComponent;
 import cz.followme.injection.AppModule;
@@ -10,6 +12,8 @@ import cz.followme.injection.DaggerAppComponent;
 import cz.followme.injection.NetworkModule;
 import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
+
+import static cz.followme.Settings.DISABLE_CRASHLYTICS_FOR_DEBUG_BUILDS;
 
 /**
  * Created by Jakub Begera (jakub@easycoreapps.com) on 07.01.17.
@@ -21,7 +25,15 @@ public class App extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        Fabric.with(this, new Crashlytics());
+        Fabric.with(
+                this,
+                new Crashlytics.Builder().core(
+                        new CrashlyticsCore.Builder()
+                                .disabled(BuildConfig.DEBUG && DISABLE_CRASHLYTICS_FOR_DEBUG_BUILDS)
+                                .build()
+                ).build(),
+                new Answers()
+        );
         initLoggers();
 
         appComponent = DaggerAppComponent.builder()
---
+++ app/src/main/java/cz/followme/Settings.java
@@ -0,0 +1,10 @@
+package cz.followme;
+
+/**
+ * Created by Jakub Begera (jakub.begera@gmail.com) on 11.01.17.
+ * (C) Copyright 2017
+ */
+
+public class Settings {
+    public static final boolean DISABLE_CRASHLYTICS_FOR_DEBUG_BUILDS = true;
+}
